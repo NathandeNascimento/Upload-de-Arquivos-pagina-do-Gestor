80da788d14bd8b04b17cfee9d906b7767e803740
NathandeNascimento <nathanoliv.nascimento@gmail.com>
2021-09-23 05:35:43 -0300

    Atualiza page Upload arquivos

--- Upload.php
+++ Upload.php
@@ -1,49 +1,58 @@
-
+ 
 <?
 require "./config.php";
 
 $msg = false;
 
-if(isset($_FILES['arquivo'])){
+if (isset($_FILES['arquivo'])) {
+
+   $telefone = $_POST['tel'];
+   $arquivo = $_FILES['arquivo']['name'];
+   $extenção = strtolower(pathinfo($arquivo, PATHINFO_EXTENSION));
+   $novo_nome = md5(time()) . "." . $extenção;
+   $diretorio = "upload/";
+
+   move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio . $novo_nome);
+   $conn = mysqli_connect($sevidor, $dbusuario, $dbsenha, $dbname);
+
+   $sql_code = "INSERT INTO arquivo (codigo, arquivo, data, telefone) VALUES('', '$novo_nome', NOW(), '$telefone')";
+
+   if (mysqli_query($conn, $sql_code))
+
+      $msg = "<script>alert('ARQUIVO ENVIADO COM SUCESSO!!!');
+      window.location = 'verificacao.php';  </script>";
+     
 
 
-    $arquivo = $_FILES['arquivo']['name'];
-    $extenção = strtolower(pathinfo($arquivo, PATHINFO_EXTENSION));
-    $novo_nome = md5(time()). "." . $extenção;
-    $diretorio = "upload/";
+   else
 
-    move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio.$novo_nome);
-    
-    $sql_code = "INSERT INTO arquivo (codigo, arquivo, data) VALUES('', '$novo_nome', NOW())";
-    
-    if(mysqli_query($conn, $sql_code))
-    
-       $msg = "Arquivo enviado com sucesso!";
-    
-    else
-    
-       $msg = "Falha ao enviar arquivo!";  
-    }
+      $msg = "Falha ao enviar arquivo!";
+}
 
- 
+
 ?>
+
+
+
+
 <html>
 <h1>Upload de arquivos</h1>
 
 <div style="color: blue ;">
-<? if(isset($msg) && $msg != false){
-        echo "<p> $msg <p/>";
-        } ?>
+   <? if (isset($msg) && $msg != false) {
+      echo "<p> $msg <p/>";
+   }
+   ?>
 
 </div>
 
-<form action="Upload.php" method="POST" enctype="multipart/form-data" >
-Arquivo <input type='file' require  name='arquivo'>
-        <input type="Submit" value="Salvar"> 
+<form action="Upload.php" method="POST" enctype="multipart/form-data">
+   Arquivo <input type='file' require name='arquivo'> <br/><br/>
+   telefone <input type="tel" require name="tel" placeholder="+123 456 789 321" ><br/>
+   <input type="Submit" value="Salvar">
 
-</form>
+</form><br/>
 
 </html>
-
 
 
