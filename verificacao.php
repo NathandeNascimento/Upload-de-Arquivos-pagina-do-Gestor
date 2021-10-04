e6105777c26dbbff3e6aa3d21f2f6757c9f17206
NathandeNascimento <nathanoliv.nascimento@gmail.com>
2021-10-04 04:32:09 -0300

    back/front pg verificacao

--- verificacao.php
+++ verificacao.php
@@ -1,3 +1,8 @@
+<?
+
+include_once "config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -9,39 +14,69 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-  <!-- HTML5Shiv -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <![endif]-->
 
   <!-- Estilo customizado -->
-  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+  <link rel="stylesheet" type="text/css" href="estil.css">
+
+
 </head>
-  
-  <? 
-  //include_once "Upload.php";
-  include_once "config.php";
-   ?>  
+
+
+
+<html>
+
 <body>
+
+  <div class="row">
+    <div class="col-md-12 text-center pt-5" style="margin-top: 0%;">
   
-</div>
-  <html>
-  <h3>Seguência Numérica</h3>
-  <form action="condicional1.php" method="post">
-    <input type="text" required name="valida">
-    <a href="condicional1.php"><input type="submit"></a>
+    </div>
+  </div>
+  
+  <div class="row">
+
+    <div class="col-md-5 d-none d-md-inline" style="margin: 132px 0 0 0;  ">
+      <img class="" src="img2/oculos.png" alt="">
+    </div>
+
+    <div class="col-md-2"></div>
+    
+    <div class="col-md-5 d-none d-lg-inline menina" style=" margin:133px 0 0 0; ">
+      <img src="img2/pessoa.png" alt="">
+    </div>
+  
+  </div>
+
+  <div class="row ">
+    
+    <div class="col-md-5 mb-3 text-center pt-5 centro ">
+
+    <p style="margin-top: -45px;margin-bottom:0px;" ><b><h1 style="color: white;">Parabens!!</h1></b></br> <b style="color: rgba(118, 218, 245, 08)">Você esta prestes a consumir o melhor conteudo musical do mundo!!!</b> </p>
+      
+    
+    
+    <form action="condicional1.php" method="post" class="input-group">
+
+        <input type="text" class="form-control" required autofocus name="valida">
+
+        <div class="input-group-append">
+          <a href="condicional1.php"> <button class="btn btn-outline-info" type="submit">Liberar Dowload</button></a>
+        </div>
+
+      </form>
+      <h5 class="fonte" style=" font-size: 16px;">Digite ou cole a seguência de caracteres que você recebeu.</h5>
+    </div>
+  </div>
+
+  
+
 
   </form>
-
-
   <!-- JavaScript (Opcional) -->
   <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+</body>
 
-  </html>
+</html>
