bd6b7c8cbe8d6b055248a5be2e9464e76735582f
NathandeNascimento <nathanoliv.nascimento@gmail.com>
2021-10-04 04:41:42 -0300

    script de Upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿'use strict'
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('Arquivo');
+
+
+file.addEventListener('change', (event) =>{
+   let reader = new FileReader();
+
+   reader.onload = () => {
+       photo.src = reader.result;
+   }
+
+    reader.readAsDataURL(file.files[0]);
+
+} );
