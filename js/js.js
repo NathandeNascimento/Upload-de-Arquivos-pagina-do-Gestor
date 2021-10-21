b887e28c6f2ccbe36611042f558c42304d33cb33
NathandeNascimento <nathanoliv.nascimento@gmail.com>
2021-10-21 05:22:08 -0300

    atualização 21-10-2021

---
+++ js/js.js
@@ -0,0 +1,20 @@
+﻿'use strict'
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('Arquivo');
+
+
+file.addEventListener('change', (event) =>{
+   let reader = new FileReader();
+
+   reader.onload = () => {
+       photo.src = reader.result;
+      
+   }
+
+    reader.readAsDataURL(file.files[0]);
+    alert("arquivo anexado com sucesso!");
+  
+   
+
+} );
